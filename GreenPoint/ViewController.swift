04de174c816b921f3b3594c79883e867a58d82b0
Hsieh <johnny.hsieh.12@gmail.com>
2022-02-02 15:17:37 +0800

    Initial Commit

---
+++ GreenPoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenPoint
+//
+//  Created by 謝承邑 on 2022/2/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
