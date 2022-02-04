a68dee5a9102aa18ac924182e8bb8257c1d324ec
Hsieh <johnny.hsieh.12@gmail.com>
2022-02-04 20:50:59 +0800

    finish tableView

--- GreenPoint/Controllers/Featured/FeaturedListViewController.swift
+++ GreenPoint/Controllers/Featured/FeaturedListViewController.swift
@@ -12,21 +12,44 @@ class FeaturedListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         featuredTableView.delegate = self
         featuredTableView.dataSource = self
     }
-
+    
+    @IBAction func cellPressed(_ sender: UIButton) {
+        guard let vc1 = storyboard?.instantiateViewController(withIdentifier: "homeViewController") else { return }
+        guard let vc3 = storyboard?.instantiateViewController(withIdentifier: "FeaturedListDetail") as? FeaturedListDetailViewController else { return }
+        
+        self.navigationController?.setViewControllers([vc1, self, vc3], animated: true)
+        
+        //計算使用者點擊了哪一個cell
+        let buttonPostion = sender.convert(sender.bounds.origin, to: featuredTableView)
+        if let indexPath = featuredTableView.indexPathForRow(at: buttonPostion) {
+            
+            //將點擊結果對應的indexpath作為數值傳入vc3的featured，用以查詢對應的文章標題、圖片
+            vc3.featured = featureds[indexPath[1]]
+        }
+    }
+    
 }
 
 extension FeaturedListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return featureds.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = featuredTableView.dequeueReusableCell(withIdentifier: K.cellIdentifier.featureTableCell, for: indexPath) as! FeaturedTableViewCell
+        
+        let featured = featureds[indexPath.item]
+        cell.banner.image = featured.image
+        cell.titleLabel.text = featured.title
+        cell.contentLabel.text = featured.content
+        cell.dateLabel.text = featured.date
+        
+        return cell
     }
     
     
--- GreenPoint/Views/FeaturedTableViewCell.swift
+++ GreenPoint/Views/FeaturedTableViewCell.swift
@@ -14,15 +14,39 @@ class FeaturedTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    override public func layoutIfNeeded() {
+        super.layoutIfNeeded()
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        baseLayer.layer.cornerRadius = 8
+        baseLayer.layer.cornerCurve = .continuous
+        baseLayer.layer.shadowOpacity = 0.15
+        baseLayer.layer.shadowOffset = CGSize(width: 0, height: 5)
+        baseLayer.layer.shadowRadius = 10
+        baseLayer.layer.masksToBounds = false
+        
+        banner.layer.cornerRadius = 8
+        banner.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    override public func prepareForReuse() {
+        super.prepareForReuse()
     }
 
 }
