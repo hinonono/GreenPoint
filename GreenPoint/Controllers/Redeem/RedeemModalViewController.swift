6c0f7b85cd47154de29e8ce90a97287642199e88
Hsieh <johnny.hsieh.12@gmail.com>
2022-02-11 16:21:00 +0800

    backup

---
+++ GreenPoint/Controllers/Redeem/RedeemModalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RedeemModalViewController.swift
+//  GreenPoint
+//
+//  Created by 謝承邑 on 2022/2/11.
+//
+
+import UIKit
+
+class RedeemModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dismissPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func redeemPressed(_ sender: UIButton) {
+//        print("Redeem button was tapped.")
+    }
+}
